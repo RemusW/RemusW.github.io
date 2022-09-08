71ff0ed95cb1461f662f9c430082fc89cd97b807
RemusW <rwong2100@hotmail.com>
2022-09-07 23:34:54 -0500

    parth

--- portfolio/src/components/button.svelte
+++ portfolio/src/components/button.svelte
@@ -4,7 +4,7 @@
     export let color = 'red'
 </script>
 
-<button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
+<button class=" bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
     on:click={() => message+='hello! '} 
     style="background: {color}; padding 1rem;">
     hello
